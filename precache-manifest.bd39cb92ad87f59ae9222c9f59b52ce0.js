924fbbc1dd25893bee1b9a75f7618ecc7858d3c2
belita25 <belita0625@gmail.com>
2019-05-08 11:17:30 +0900

    Updates

---
+++ precache-manifest.bd39cb92ad87f59ae9222c9f59b52ce0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd74ebf6f2e5b07dbf9889e5395a107d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "db3671489b6c97d44e94",
+    "url": "/movie_app/static/css/main.7a0f3428.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "db3671489b6c97d44e94",
+    "url": "/movie_app/static/js/main.e93a34b3.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
